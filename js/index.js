76a8dc7c81a4ad08f629dce22dc9fe0c54478998
Alan Jones <aljones1816@gmail.com>
2020-04-22 22:08:53 -0400

    created a js function to create a grid

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function addDivs(numCols) {
+    var container = document.getElementById("container");
+    container.style.gridTemplateColumns = "repeat(" + numCols + ",1fr)";
+    for (let i=0;i<numCols * numCols;i++) {
+        var newDiv = document.createElement("div");
+        newDiv.classList.add("pixel");
+        newDiv.innerHTML = "hi"
+        container.appendChild(newDiv);
+    }
+
+}
+
+addDivs(8);
